6cc568125a174d5042c1571d5b16c36465c58e5a
dxstanley <support@devexpress.com>
2019-09-03 17:18:33 +0300

    update example

--- VB/Default.aspx
+++ VB/Default.aspx
@@ -1,33 +1,42 @@
-﻿<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
-
-<%@ Register assembly="DevExpress.Web.ASPxRichEdit.v16.2, Version=16.2.3.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.Web.ASPxRichEdit" tagprefix="dx" %>
-
-<%@ Register assembly="DevExpress.Web.v16.2, Version=16.2.3.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.Web" tagprefix="dx" %>
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-	<title></title>
-	 <script>
-	  var startPosition = -1;
-	  function OnClick(s, e) {
-	   startPosition = rich.selection.intervals[0].start;
-	   rich.PerformCallback(startPosition);
-	  }
-	</script>
+    <title>ASPxRichEdit - How to insert RTF text to a document</title>
+    <script type="text/javascript">
+        function onClientButtonClick(s, e) {
+            var startPosition = richEdit.selection.intervals[0].start;
+            var rtf = richEdit.cpRtf;
+            richEdit.commands.insertRtf.execute(rtf, startPosition);
+        }
+
+        function onServerButtonClick(s, e) {
+            var startPosition = richEdit.selection.intervals[0].start;
+            richEdit.PerformCallback(startPosition);
+        }
+    </script>
 </head>
 <body>
-	<form id="form1" runat="server">
-	 <dx:ASPxRichEdit ID="ASPxRichEdit1" ClientInstanceName="rich" runat="server" WorkDirectory="~\App_Data\WorkDirectory" OnCallback="ASPxRichEdit1_Callback">
-	  <Settings>
-	   <Behavior CreateNew="Hidden" Save="Hidden" Open="Hidden" SaveAs="Hidden" />
-	  </Settings>
-	 </dx:ASPxRichEdit>
+    <form id="form1" runat="server">
+    <div>
+        <dx:ASPxRichEdit ID="ASPxRichEdit1" ClientInstanceName="richEdit" runat="server" WorkDirectory="~\App_Data\WorkDirectory" 
+            OnCallback="ASPxRichEdit1_Callback"
+            OnCustomJSProperties="ASPxRichEdit1_CustomJSProperties">
+            <Settings>
+                <Behavior CreateNew="Hidden" Save="Hidden" Open="Hidden" SaveAs="Hidden" />
+            </Settings>
+        </dx:ASPxRichEdit>
 
-	 <dx:ASPxButton runat="server" ID="addButton" Text="Insert formatted RTF text" AutoPostBack="false">
-	  <ClientSideEvents Click="OnClick" />
-	 </dx:ASPxButton>
-	</form>
+        <dx:ASPxButton runat="server" ID="clientButton" Text="Insert RTF text on the client side" AutoPostBack="false">
+            <ClientSideEvents Click="onClientButtonClick" />
+        </dx:ASPxButton>
+
+        <dx:ASPxButton runat="server" ID="serverButton" Text="Insert RTF text on the server side" AutoPostBack="false">
+            <ClientSideEvents Click="onServerButtonClick" />
+        </dx:ASPxButton>
+    </div>
+    </form>
 </body>
 </html>
--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -1,32 +1,25 @@
-﻿Imports Microsoft.VisualBasic
-Imports DevExpress.XtraRichEdit
-Imports System
-Imports System.Collections.Generic
+﻿
 Imports System.IO
-Imports System.Linq
-Imports System.Web
-Imports System.Web.UI
-Imports System.Web.UI.WebControls
+Imports DevExpress.XtraRichEdit
 
-Partial Public Class _Default
-	Inherits System.Web.UI.Page
-	Private rtf As String = "{\rtf1\deff0{\fonttbl{\f0 Calibri;}{\f1 Times New Roman;}}{\colortbl ;\red0\green0\blue255 ;\red255\green0\blue0 ;}{\*\defchp \f1}{\stylesheet {\ql\f1 Normal;}{\*\cs1\f1 Default Paragraph Font;}{\*\cs2\sbasedon1\f1 Line Number;}{\*\cs3\ul\f1\cf1 Hyperlink;}{\*\ts4\tsrowd\f1\ql\tscellpaddfl3\tscellpaddl108\tscellpaddfb3\tscellpaddfr3\tscellpaddr108\tscellpaddft3\tsvertalt\cltxlrtb Normal Table;}{\*\ts5\tsrowd\sbasedon4\f1\ql\trbrdrt\brdrs\brdrw10\trbrdrl\brdrs\brdrw10\trbrdrb\brdrs\brdrw10\trbrdrr\brdrs\brdrw10\trbrdrh\brdrs\brdrw10\trbrdrv\brdrs\brdrw10\tscellpaddfl3\tscellpaddl108\tscellpaddfr3\tscellpaddr108\tsvertalt\cltxlrtb Table Simple 1;}}{\*\listoverridetable}{\info{\operator Humberg, Gerrit}{\creatim\yr2017\mo1\dy16\hr15\min31}{\revtim\yr2017\mo1\dy17\hr15\min4}{\version4}}\nouicompat\splytwnine\htmautsp\sectd\marglsxn284\margrsxn284\margtsxn0\margbsxn0\pard\plain\ql\sl275\slmult1\sa200{\fs22\cf0 This is the sample text with }{\b\fs22\cf0 bold }{\fs22\cf0 and }{\fs22\cf2 red}{\fs22\cf0  wor}{\fs22\cf0 ds.}\lang7\langfe7\fs22\par}"
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+Partial Class _Default
+    Inherits System.Web.UI.Page
+    Const rtf As String = "{\rtf1\deff0{\fonttbl{\f0 Calibri;}{\f1 Times New Roman;}}{\colortbl ;\red0\green0\blue255 ;\red255\green0\blue0 ;}{\*\defchp \f1}{\stylesheet {\ql\f1 Normal;}{\*\cs1\f1 Default Paragraph Font;}{\*\cs2\sbasedon1\f1 Line Number;}{\*\cs3\ul\f1\cf1 Hyperlink;}{\*\ts4\tsrowd\f1\ql\tscellpaddfl3\tscellpaddl108\tscellpaddfb3\tscellpaddfr3\tscellpaddr108\tscellpaddft3\tsvertalt\cltxlrtb Normal Table;}{\*\ts5\tsrowd\sbasedon4\f1\ql\trbrdrt\brdrs\brdrw10\trbrdrl\brdrs\brdrw10\trbrdrb\brdrs\brdrw10\trbrdrr\brdrs\brdrw10\trbrdrh\brdrs\brdrw10\trbrdrv\brdrs\brdrw10\tscellpaddfl3\tscellpaddl108\tscellpaddfr3\tscellpaddr108\tsvertalt\cltxlrtb Table Simple 1;}}{\*\listoverridetable}{\info{\operator Humberg, Gerrit}{\creatim\yr2017\mo1\dy16\hr15\min31}{\revtim\yr2017\mo1\dy17\hr15\min4}{\version4}}\nouicompat\splytwnine\htmautsp\sectd\marglsxn284\margrsxn284\margtsxn0\margbsxn0\pard\plain\ql\sl275\slmult1\sa200{\fs22\cf0 This is the sample text with }{\b\fs22\cf0 bold }{\fs22\cf0 and }{\fs22\cf2 red}{\fs22\cf0  wor}{\fs22\cf0 ds.}\lang7\langfe7\fs22\par}"
+    Protected Sub ASPxRichEdit1_Callback(sender As Object, e As DevExpress.Web.CallbackEventArgsBase)
+        Dim memoryStream = New MemoryStream()
+        ASPxRichEdit1.SaveCopy(memoryStream, DocumentFormat.Rtf)
+        memoryStream.Position = 0
 
-	End Sub
-	Protected Sub ASPxRichEdit1_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.CallbackEventArgsBase)
-		Dim memoryStream As New MemoryStream()
-		ASPxRichEdit1.SaveCopy(memoryStream, DocumentFormat.Rtf)
-		memoryStream.Position = 0
+        Dim server = New RichEditDocumentServer()
+        server.LoadDocument(memoryStream, DocumentFormat.Rtf)
+        Dim pos = server.Document.CreatePosition(Convert.ToInt32(e.Parameter))
+        server.Document.InsertRtfText(pos, rtf)
 
-		Dim server = New RichEditDocumentServer()
-		server.LoadDocument(memoryStream, DocumentFormat.Rtf)
-		Dim pos = server.Document.CreatePosition(Convert.ToInt32(e.Parameter))
-		server.Document.InsertRtfText(pos, rtf)
-
-		memoryStream = New MemoryStream()
-		server.SaveDocument(memoryStream, DocumentFormat.Rtf)
-		ASPxRichEdit1.Open(Guid.NewGuid().ToString(), DocumentFormat.Rtf, Function() memoryStream.ToArray())
-
-	End Sub
+        memoryStream = New MemoryStream()
+        server.SaveDocument(memoryStream, DocumentFormat.Rtf)
+        ASPxRichEdit1.Open(Guid.NewGuid().ToString(), DocumentFormat.Rtf, Function() memoryStream.ToArray())
+    End Sub
+    Protected Sub ASPxRichEdit1_CustomJSProperties(sender As Object, e As DevExpress.Web.CustomJSPropertiesEventArgs)
+        e.Properties("cpRtf") = rtf
+    End Sub
 End Class
